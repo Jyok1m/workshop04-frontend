4aab1920a3d79b90e9ae396d422ac74bad4cd6d9
Joachim Alexandre Jasmin <joachim.jasmin@gmail.com>
2024-04-12 17:05:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateTodaysDate() {
+	return fetch("https://workshop04-backend.vercel.app/date")
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.result === true) {
+				document.querySelector("#date").textContent = data.today;
+			}
+		});
+}
+
+generateTodaysDate();
